b8098cf676d80dc8eb6bf482b7b952bb6904aadf
Hitesh Saini <hiteshsaini57520@gmail.com>
2022-10-30 15:34:37 +0530

    code updated

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "https://movie5backend.herokuapp.com/",
+  baseURL: "https://movie5backend.herokuapp.com/api",
 });
 
 export default client;
